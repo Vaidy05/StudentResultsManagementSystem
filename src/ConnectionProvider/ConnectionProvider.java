94d56630220db9513edd77917877a800a5eafced
Vaidy05 <>
2022-10-25 17:46:24 +0530

    Student Results Management System

---
+++ src/ConnectionProvider/ConnectionProvider.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConnectionProvider;
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Intel
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/StudentResultSystem","root","Balaji00!");
+            return con;
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,e.toString());
+            return null;
+        }
+    }
+}
